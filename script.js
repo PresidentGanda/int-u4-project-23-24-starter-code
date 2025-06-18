ed6871b368cc19e0099963dcb8234263581a1e12
PresidentGanda <solomon.bennett55@gmail.com>
2025-06-18 14:43:45 -0400

    final

--- script.js
+++ script.js
@@ -4,59 +4,82 @@ let button3 = document.querySelector(".three");
 let button4 = document.querySelector(".four");
 let button5 = document.querySelector(".five");
 let button6 = document.querySelector(".six");
-let answer1 = 0;
-let answer2 = 0;
-let answer3 = 0;
+let maul = 0;
+let vader = 0;
+let grevous = 0;
+let dooku = 0;
 let result = document.querySelector(".result");
-
+let pic1 = document.querySelector(".mau");
+let pic2 = document.querySelector(".vad");
+let pic3 = document.querySelector(".gre");
+let pic4 = document.querySelector(".doo");
 button1.addEventListener("click", function() {
-answer1 = 1;
+maul+=2;
+vader+=2;
 });
 button2.addEventListener("click", function() {
-answer1 = 2;
+grevous+=2;
+dooku+=2;
 });
 button3.addEventListener("click", function() {
-answer2 = 1;
+grevous++;
+vader++;
 });
 button4.addEventListener("click", function() {
-answer2 = 2;
+dooku ++;
+maul++;
 });
 button5.addEventListener("click", function() {
-answer3 = 1;
-// result.style.display = "block";
-if(answer1 == 1 && answer2 ==2 && answer3 ==1) {
+dooku+=2;
+maul+=2;
+ result.style.display = "block";
+if(maul > dooku && maul > vader && maul > grevous) {
 result.innerHTML = "Your sith is Darth Maul";
+pic1.style.display = "block";
 }
-if (answer1 == 1 && answer2 == 1 && answer3 == 2){
+else if (vader > dooku && vader > maul && vader > grevous){
 result.innerHTML = "Your sith is Darth Vader";
+pic2.style.display = "block";
 }
 
-else if(answer1 == 2 && answer2 ===1 && answer3 === 2){
-result.innerHTML = "Your sith is General Grevous";
+else if(grevous > maul && grevous > vader && grevous > dooku){
+result.innerHTML = "Your sith is General Grievous";
+pic3.style.display = "block";
 }
 
-else if(answer1 == 2 && answer2 ===2 && answer3 === 1){
+else if(dooku > vader && dooku > grevous && dooku>maul){
 result.innerHTML = "Your sith is Count Dooku";
+pic4.style.display = "block";
 }
+else
+result.innerHTML = "Your sith is Darth Vader";
+//pic2.style.display = "block";
 });
 button6.addEventListener("click", function() {
-answer3 = 2;
-// result.style.display = "block";
-if(answer1 == true) {
+grevous+=2;
+vader+=2;
+ result.style.display = "block";
+if(maul > dooku && maul > vader && maul > grevous) {
 result.innerHTML = "Your sith is Darth Maul";
+pic1.style.display = "block";
 }
-if (answer1 == 1 && answer3 == 2){
+else if (vader > dooku && vader > maul && vader > grevous){
 result.innerHTML = "Your sith is Darth Vader";
+pic2.style.display = "block";
 }
 
-else if(answer2 ===1 && answer3 === 2){
-result.innerHTML = "Your sith is General Grevous";
+else if(grevous > maul && grevous > vader && grevous > dooku){
+result.innerHTML = "Your sith is General Grievous";
+pic3.style.display = "block";
 }
 
-else if(answer2 ===2 && answer3 === 2){
+else if(dooku > vader && dooku > grevous && dooku>maul){
 result.innerHTML = "Your sith is Count Dooku";
+pic4.style.display = "block";
 }
+else
+result.innerHTML = "Your sith is Darth Vader";
+//pic2.style.display = "block";
 });
 
-// result.innerHTML = answer1;
 
